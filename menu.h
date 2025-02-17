b02d17e9525fffa018ab09d42963e615cd04a41c
张文兵 <wenbing@mgtv.com>
2025-02-17 13:08:17 +0800

    实现简单的MacOS的菜单栏和状态栏功能

---
+++ menu.h
@@ -0,0 +1,21 @@
+//
+// Created by 张文兵 on 2025/2/14.
+//
+
+#ifndef APP_MENU_H
+#define APP_MENU_H
+
+class Menu {
+public:
+    Menu();  // 构造函数
+    ~Menu(); // 析构函数
+
+    // 创建菜单
+    void createMenu();
+    void createSystemTray();
+
+    static void customAction();
+    // id mainMenu;  // 存储主菜单（作为成员变量）
+};
+
+#endif // APP_MENU_H
---
+++ menu.mm
@@ -0,0 +1,91 @@
+#include "menu.h"
+#import <Cocoa/Cocoa.h>
+#include  <iostream>
+
+// Objective-C 类定义（直接在 .mm 文件中实现）
+@interface TrayHandler : NSObject
++ (void)onCustomAction:(id)sender;
+@end
+
+@implementation TrayHandler
++ (void)onCustomAction:(id)sender { Menu::customAction(); }
+@end
+
+Menu::Menu() {
+}
+
+Menu::~Menu() {
+}
+
+void Menu::createMenu() {
+    // 创建主菜单
+    NSMenu *mainMenu = [NSMenu new];
+
+    // 创建应用程序菜单
+    NSMenuItem *appMenuItem = [NSMenuItem new];
+    NSMenu *appMenu = [NSMenu new];
+    [appMenu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
+    [appMenuItem setSubmenu:appMenu];
+    [mainMenu addItem:appMenuItem];
+
+    // 创建编辑菜单
+    NSMenuItem *editMenuItem = [[NSMenuItem alloc] initWithTitle:@"Edit"
+    action:nil
+    keyEquivalent:@""];
+    NSMenu *editMenu = [[NSMenu alloc] initWithTitle:@"Edit"];
+    [editMenu addItemWithTitle:@"Undo" action:@selector(undo:) keyEquivalent:@"z"];
+    [editMenu addItemWithTitle:@"Redo" action:@selector(redo:) keyEquivalent:@"y"];
+    [editMenu addItem:[NSMenuItem separatorItem]];
+    [editMenu addItemWithTitle:@"Cut" action:@selector(cut:) keyEquivalent:@"x"];
+    [editMenu addItemWithTitle:@"Copy" action:@selector(copy:) keyEquivalent:@"c"];
+    [editMenu addItemWithTitle:@"Paste" action:@selector(paste:) keyEquivalent:@"v"];
+    [editMenu addItemWithTitle:@"Select All" action:@selector(selectAll:) keyEquivalent:@"a"];
+    [editMenuItem setSubmenu:editMenu];
+    [mainMenu addItem:editMenuItem];
+
+    // 设置应用程序的主菜单
+    [NSApp setMainMenu:mainMenu];
+
+    // 确保主菜单已更新
+    [NSApp updateWindows];
+}
+
+void Menu::createSystemTray() {
+    // 获取状态栏实例
+    NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
+
+    // 创建状态栏项目
+    NSStatusItem *statusItem = [statusBar statusItemWithLength:NSVariableStatusItemLength];
+
+    // 设置状态栏图标的图像
+    NSImage *image = [[NSImage alloc] initWithSize:NSMakeSize(18, 18)];
+    [image lockFocus];
+    [[NSColor blackColor] set];
+    NSBezierPath *path = [NSBezierPath bezierPathWithOvalInRect:NSMakeRect(0, 0, 18, 18)];
+    [path fill];
+    [image unlockFocus];
+    [statusItem setImage:image];
+
+     // 创建菜单并绑定 target
+    NSMenu *trayMenu = [[NSMenu alloc] initWithTitle:@""];
+
+    // 添加菜单项并设置 target
+    NSMenuItem *showItem = [trayMenu addItemWithTitle:@"自定义操作"
+                                              action:@selector(onCustomAction:)
+                                       keyEquivalent:@""];
+    [showItem setTarget:[TrayHandler class]];  // 关键：设置 target 为 TrayHandler 类
+
+
+    [trayMenu addItem:[NSMenuItem separatorItem]];
+    [trayMenu addItemWithTitle:@"退出"
+                       action:@selector(terminate:)
+                keyEquivalent:@"q"];
+
+    // 将菜单设置为状态栏项目的子菜单
+    [statusItem setMenu:trayMenu];
+}
+
+void Menu::customAction()
+{
+    std::cout << "C++: Custom Action\n";
+}
